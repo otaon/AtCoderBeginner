207cd479143d2e446ebd924431553c74e5f41580
otaon <16165624+otaon@users.noreply.github.com>
2019-02-16 19:06:31 +0900

    refactor: fix redundant code

--- GrandGarden.lisp
+++ GrandGarden.lisp
@@ -1,45 +1,48 @@
-;;; C - Grand Garden
+;;;; C - Grand Garden
 
-;;; -*- 問題文 -*-{{{
-;;; 問題文
-;;; 花壇にN本の花が咲いており、それぞれ 1, 2, ......, N と番号が振られています。
-;;; 最初、全ての花の高さは 0 です。
-;;; 数列 h={h1, h2, h3, ......} が入力として与えられます。
-;;; 以下の「水やり」操作を繰り返すことで、すべての k(1≦k≦N) に対して花kの高さをh_kにしたいです。
-;;;   - 整数 l,rを指定する。l≦x≦r を満たすすべてのxに対して、花 x の高さを1高くする。
-;;; 条件を満たすための最小の「水やり」操作の回数を求めてください。
+; -*- 問題文 -*-{{{
 
-;;; 制約
-;;; - 1≦N≦100
-;;; - 0≦hi≦100
-;;; - 入力はすべて整数である。
+; 問題文
+; 花壇にN本の花が咲いており、それぞれ 1, 2, ......, N と番号が振られています。
+; 最初、全ての花の高さは 0 です。
+; 数列 h={h1, h2, h3, ......} が入力として与えられます。
+; 以下の「水やり」操作を繰り返すことで、すべての k(1≦k≦N) に対して花kの高さをh_kにしたいです。
+;   - 整数 l,rを指定する。l≦x≦r を満たすすべてのxに対して、花 x の高さを1高くする。
+; 条件を満たすための最小の「水やり」操作の回数を求めてください。
 
-;;; 入力
-;;; 入力は以下の形式で標準入力から与えられます。
-;;;
-;;; ```
-;;; N
-;;; h_1 h_2 h_3 ...... h_N
-;;; ```
+; 制約
+; - 1≦N≦100
+; - 0≦hi≦100
+; - 入力はすべて整数である。
 
-;;; 出力
-;;; 条件を満たすような最小の「水やり」操作の回数を出力してください。
-;;;}}}
+; 入力
+; 入力は以下の形式で標準入力から与えられます。
+;
+; ```
+; N
+; h_1 h_2 h_3 ...... h_N
+; ```
+
+; 出力
+; 条件を満たすような最小の「水やり」操作の回数を出力してください。
+
+;}}}
 
 
 ;;; -*- 本体 -*-{{{
+
 (defun solve (stream)
   "問題を解く
    stream 入力"
-  (let ((flower-num (read stream))
-        (waterings 0))
-    (labels ((count-watering (current height &optional (last-height 0))
+  (let ((flower-num (read stream)))
+    (labels ((count-watering (waterings current height &optional (last-height 0))
                (if (< height last-height)
                    (setf waterings (+ waterings (- last-height height))))
                (if (< current flower-num)
-                   (count-watering (1+ current) (read stream) height)
+                   (count-watering waterings (1+ current) (read stream) height)
                    (+ waterings height))))
-      (count-watering 1 (read stream)))))
+      (count-watering 0 1 (read stream)))))
+
 ;}}}
 
 
@@ -89,7 +92,7 @@
 (defun test-solve (test-data-path expected-data-path)
   "テスト
    test-data-path テストデータのパス
-   expected-data-path 期待値データのパス"
+  expected-data-path 期待値データのパス"
   (with-open-file (in-test test-data-path)
     (with-open-file (in-expected expected-data-path)
       (let ((actual (solve in-test))
@@ -102,15 +105,16 @@
 
 
 ;;; 全テストデータに対してテスト実施
-(if (member nil
-            (mapcar (lambda (test-data expected-data)
-                      (princ "test data:     ") (princ test-data) (princ #\newline)
-                      (princ "expected data: ") (princ expected-data) (princ #\newline)
-                      (test-solve
-                        (merge-pathnames *test-data-dir* test-data)
-                        (merge-pathnames *expected-data-dir* expected-data)))
-                    *test-data-list*
-                    *expected-data-list*))
-    (princ "NG")
-    (princ "OK"))
+(if (every (lambda (data)
+             (let ((test-data (first data))
+                   (expected-data (second data)))
+               (princ "test data:     ") (princ test-data) (princ #\newline)
+               (princ "expected data: ") (princ expected-data) (princ #\newline)
+               (test-solve
+                 (merge-pathnames *test-data-dir* test-data)
+                 (merge-pathnames *expected-data-dir* expected-data))))
+           (apply #'mapcar #'list (list *test-data-list* *expected-data-list*)))
+    (princ "OK")
+    (princ "NG"))
+
 ;}}}
